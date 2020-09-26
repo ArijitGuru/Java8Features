f4a0602036a8a8e11252aa3c33a17acf64fffd88
arijit adhikary <arijitadhikary@gmail.com>
2020-09-26 10:18:06 -0400

    updated

--- src/com/arijit/forEach/ForEachExample.java
+++ src/com/arijit/forEach/ForEachExample.java
@@ -1,6 +1,27 @@
 package com.arijit.forEach;
 
-public class ForEachExample {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ForEachExample {
+	
+	public static void main(String args[]) {
+		List<String> nameList = new ArrayList<>();
+		
+		for (int  i = 0; i < 10; i++) {
+			String name = "Arijit_"+ i;
+			nameList.add(name);
+			
+		}
+		
+		nameList.forEach(item -> {
+			System.out.println(item);
+		});
+		
+		
+		
+		
+	}
+	
 	
 }
