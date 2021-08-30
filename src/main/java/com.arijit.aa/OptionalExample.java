4751176e9e5a9481c3e3c30fb2b2e387cbbd30be
arijit adhikary <arijitadhikary@gmail.com>
2021-08-30 12:16:55 -0400

    Optional Class with Lambda hands-on

---
+++ src/main/java/com.arijit.aa/OptionalExample.java
@@ -0,0 +1,26 @@
+package com.arijit.aa;
+
+import java.util.Optional;
+
+public class OptionalExample {
+
+    public static void main(String[] args) throws Exception {
+        Person personA = new Person("Arijit", "Charlotte", "987654");
+        Person personB = new Person("Abhra", null, "987654");
+
+        //print city name in uppercase
+        //System.out.println("City in uppercase : " + personB.getCity().toUpperCase()); // throws NPE
+
+        //Avoid NPE
+        Optional<String> opCity = Optional.ofNullable(personB.getCity());
+        System.out.println(opCity.orElse("Default value"));
+
+        //Throw Exception
+        System.out.println(opCity
+                .orElseThrow(() -> new Exception("Value is null")));
+
+
+        //Recommended
+        //Add Optional<String> in VO or POJO getters() instead of using primitive String
+    }
+}
